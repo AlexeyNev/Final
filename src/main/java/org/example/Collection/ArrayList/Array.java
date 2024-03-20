57017479881ddc0264c72133d70863d6e758a63a
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2024-03-20 22:28:35 +0400

    Array

---
+++ src/main/java/org/example/Collection/ArrayList/Array.java
@@ -0,0 +1,14 @@
+package org.example.Collection.ArrayList;
+
+import java.util.ArrayList;
+
+public class Array {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Alex");
+        arrayList.add("Alla");
+        arrayList.add("Mars");
+
+        System.out.println(arrayList);
+    }
+}
